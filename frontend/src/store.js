bbdc321f4fce1b4439e0b7689c8c533799ce91ff
mishraankit-1497 <ankitkumarmishra14121997@gmail.com>
2021-09-10 14:20:24 +0530

    Single product details using redux implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productReducers } from "./reducers/productReducers";
+import {
+  productDetailsReducers,
+  productReducers,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
   productList: productReducers,
+  productDetails: productDetailsReducers,
 });
 
 const initialState = {};
