39eb4909dd63aaf1ec41a57fc7b2050d56dfa2ab
mishraankit-1497 <ankitkumarmishra14121997@gmail.com>
2021-10-01 19:09:03 +0530

    Search box implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,6 +38,7 @@ const App = () => {
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
